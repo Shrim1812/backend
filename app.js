c1a9cab55684d3eea61d403621d1dc0aa090f43e
Shrim1812 <spaldhikar840@gmail.com>
2025-06-18 14:10:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extends: true }));
-app.use("/otpRouter", otpRouter);
+// app.use("/otpRouter", otpRouter);
 
 app.use("/Ohkla", Member);
 app.use((req, res, next) => {
